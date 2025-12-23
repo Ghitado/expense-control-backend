8d70c83b64f0c0999be15b51ddd26d139b1c10eb
Ghitado <thiaguinho.020405@gmail.com>
2025-12-23 00:37:49 -0300

    chore(infra): Update dependency injection and remove obsolete report services

--- src/ExpenseControl.Infrastructure/DependencyInjection.cs
+++ src/ExpenseControl.Infrastructure/DependencyInjection.cs
@@ -1,9 +1,11 @@
 ﻿using ExpenseControl.Domain.Interfaces;
 using ExpenseControl.Domain.Interfaces.Repositories;
-using ExpenseControl.Domain.Interfaces.Services;
+using ExpenseControl.Domain.Interfaces.Security.Cryptography;
+using ExpenseControl.Domain.Interfaces.Security.Tokens;
 using ExpenseControl.Infrastructure.Persistence;
-using ExpenseControl.Infrastructure.Repositories;
-using ExpenseControl.Infrastructure.Services;
+using ExpenseControl.Infrastructure.Persistence.Repositories;
+using ExpenseControl.Infrastructure.Security.Cryptography;
+using ExpenseControl.Infrastructure.Security.Tokens;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -15,17 +17,29 @@ public static class DependencyInjection
 	public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
 	{
 		return services
-			.AddDatabaseConnection(configuration)
-			.AddRepositories()
-			.AddServices();
+			.AddDatabase(configuration)
+			.AddSecurity(configuration)
+			.AddRepositories();
 	}
 
-	private static IServiceCollection AddDatabaseConnection(this IServiceCollection services, IConfiguration configuration)
+	private static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration)
 	{
 		var connectionString = configuration.GetConnectionString("DefaultConnection");
 
 		services.AddDbContext<ExpenseControlDbContext>(options =>
 			options.UseNpgsql(connectionString));
+
+		services.AddScoped<DbInitializer>();
+
+		return services;
+	}
+
+	private static IServiceCollection AddSecurity(this IServiceCollection services, IConfiguration configuration)
+	{
+		services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.SectionName));
+
+		services.AddSingleton<IPasswordHasher, BCryptPasswordHasher>();
+		services.AddSingleton<ITokenService, JwtTokenService>();
 
 		return services;
 	}
@@ -33,16 +47,12 @@ public static class DependencyInjection
 	private static IServiceCollection AddRepositories(this IServiceCollection services)
 	{
 		return services
+			.AddScoped<IUserRepository, UserRepository>()
+			.AddScoped<IRefreshTokenRepository, RefreshTokenRepository>()
 			.AddScoped<IPersonRepository, PersonRepository>()
 			.AddScoped<ICategoryRepository, CategoryRepository>()
 			.AddScoped<ITransactionRepository, TransactionRepository>()
+			.AddScoped<IReportRepository, ReportRepository>()
 			.AddScoped<IUnitOfWork, UnitOfWork>();
-	}
-
-	private static IServiceCollection AddServices(this IServiceCollection services)
-	{
-		return services
-			.AddScoped<IPersonReportService, PersonReportService>()
-			.AddScoped<ICategoryReportService, CategoryReportService>();
 	}
 }
